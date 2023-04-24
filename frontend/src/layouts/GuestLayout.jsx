b4547418c7e5c88cb2eb1e627b9e36b323c739a7
alfariiizi <alfariiiziiiii@gmail.com>
2023-04-25 05:46:25 +0700

    Creating multiple layout (component)

---
+++ frontend/src/layouts/GuestLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const GuestLayout = () => {
+  return (
+    <div>
+      <p>GuestLayout</p>
+      <Outlet />
+    </div>
+  );
+};
+
+export default GuestLayout;
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,26 +1,48 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Users from "./pages/Users";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./pages/Dashboard";
 
 const router = createBrowserRouter([
+  // For authenticated user
   {
     path: "/",
-    element: <Home />,
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to={"/users"} />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+    ],
   },
+
+  // For non authenticated user (guest)
   {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/users",
-    element: <Users />,
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
   },
   {
     path: "*",
