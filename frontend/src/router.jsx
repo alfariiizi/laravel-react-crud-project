64e74293e2c086770d717b8c7a3ae712604a1a02
alfariiizi <alfariiiziiiii@gmail.com>
2023-04-25 06:11:48 +0700

    Creating Context API and use it for protected routes

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -4,8 +4,8 @@ import Users from "./pages/Users";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
-import DefaultLayout from "./components/DefaultLayout";
-import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./layouts/DefaultLayout";
+import GuestLayout from "./layouts/GuestLayout";
 import Dashboard from "./pages/Dashboard";
 
 const router = createBrowserRouter([
